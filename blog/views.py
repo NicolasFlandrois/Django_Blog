4733cfb184015399ea215fb6bd2d49698ada6372
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-10-22 21:37:55 +0200

    Templates - Create / Initiate - Django displays Static templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'NFL',
+        'title': 'Blog Post 1',
+        'content': 'First Blog Post content Blabla Pouloulou',
+        'date_posted': 'Oct 22, 2019'
+    },
+    {
+        'author': 'JDoe',
+        'title': 'Blog Post 2',
+        'content': 'PouloulouPouloulouPouloulouPouloulouPouloulouPouloulou',
+        'date_posted': 'Oct 23, 2019'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1> Blog About </h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
