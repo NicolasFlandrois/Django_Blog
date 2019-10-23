10433bcf274f126d2052f36913698c382301bfb0
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-10-23 22:18:57 +0200

    Django Migration + Models update
    python manage.py makemigrations
    python manage.py migrate

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,9 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     """docstring for Post"""
     title = models.CharField(max_length=100)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
