24dc1f1fd3e8b777fc870ef66c4d25641bef5865
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-10-23 22:19:41 +0200

    Removed Dummy Data
    Will build further on to search data from DB

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
 
 
-posts = [
-    {
-        'author': 'NFL',
-        'title': 'Blog Post 1',
-        'content': 'First Blog Post content Blabla Pouloulou',
-        'date_posted': 'Oct 22, 2019'
-    },
-    {
-        'author': 'JDoe',
-        'title': 'Blog Post 2',
-        'content': 'PouloulouPouloulouPouloulouPouloulouPouloulouPouloulou',
-        'date_posted': 'Oct 23, 2019'
-    }
-]
-
-
 def home(request):
     context = {
         'posts': posts
@@ -25,4 +9,4 @@ def home(request):
 
 
 def about(request):
-    return render(request, 'blog/about.html', {'title':'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
