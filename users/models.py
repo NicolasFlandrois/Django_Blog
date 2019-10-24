ba0b76d0be77bb01cd0a5d05df6ab216fd3145cb
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-10-24 18:02:24 +0200

    Profile & Image
    Add a profile & default image to users, when a user is created.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    # Add here more field (added on top of User's model) to appear in profile e.g. Bio, City, langage, etc
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
