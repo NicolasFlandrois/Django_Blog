457037674a4609a8ead57198b04f3164f968e674
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-10-25 21:56:06 +0200

    Delete Post View
    Managing Identified (logged in user) author validation, Author only can delete.
    Managing deletion & redirect to Home page

--- blog/urls.py
+++ blog/urls.py
@@ -17,7 +17,8 @@ from django.urls import path
 from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
-                    PostUpdateView)
+                    PostUpdateView,
+                    PostDeleteView)
 from . import views
 
 urlpatterns = [
@@ -26,5 +27,7 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(),
          name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(),
+         name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
