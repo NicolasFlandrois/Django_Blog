34eca68311dea48755c625eac2632c2f1f851bcf
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-10-28 21:28:34 +0100

    Reset Password
    Possibility to change or reset a password using Django tools, sending an email to the user with a uidb86 and a token, to reset password, then can change pw

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,14 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+with open('config.json') as f:
+    config = json.load(f)
+
+EMAIL_HOST_USER = config['EMAIL_USER']
+EMAIL_HOST_PASSWORD = config['EMAIL_PASSWORD']
